f885188aeb5e3df3abecb1009271451e7ca1ce06
qcgood <393667566@qq.com>
2018-07-20 17:24:29 +0800

    20号考闻bug

--- pages/canvas/canvas.js
+++ pages/canvas/canvas.js
@@ -176,14 +176,12 @@ Page({
           others: [{
             key: "/images/liulan.png",
             value: res.data.data.visitQuantity
-
           }, {
             key: "/images/comments.png",
             value: "111"
           }, {
             key: "/images/dianzan.png",
             value: res.data.data.praiseQuantity
-
           }, {
             key: "/images/share.png",
             value: "111"
@@ -288,7 +286,8 @@ Page({
     ctx2.setFillStyle("#ccc");
     ctx2.fillText("地址:" + that.data.userData.address, width * 0.15 - 20, height * 0.28 + that.data.line * 16 + 40 + 20);
     for (let i = 0; i < that.data.userData.others.length; i++) {
-      ctx2.drawImage(that.data.userData.others[i].key, width * 0.15 - 20 + i * (60 + width * 0.025), height * 0.28 + that.data.line * 16 + 40 + 30, width * 0.025, height * 0.025);
+      console.log('======>', that.data.userData.others[i].key)
+      ctx2.drawImage(that.data.userData.others[i].key, width * 0.15 - 20 + i * (60 + width * 0.025), height * 0.28 + that.data.line * 16 + 40 + 30, 15, 15);
       ctx2.fillText(that.data.userData.others[i].value, width * 0.15 - 20 + i * (60 + width * 0.03) + width * 0.031 + 5, height * 0.28 + that.data.line * 16 + 40 + 43, width * 0.03, height * 0.03);
     }
     ctx2.drawImage(that.data.erweima, width - 80, height * 0.85 - 80, 60, 60);
